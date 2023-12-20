69aba01551daa31f125c161868303e8a83d9b8c8
Jeremiah-Olatunde <jesuseun.j.olatunde@gmail.com>
2023-12-20 16:28:55 +0100

    problem 9 solutions

---
+++ problem-9.hs
@@ -0,0 +1,10 @@
+-- Pack consecutive duplicates of list elements into sublists.
+import Data.List
+
+
+pack :: Eq a => [a] -> [[a]]
+pack [] = []
+pack (x:xs) = (x : takeWhile (== x) xs) : pack (dropWhile (== x) xs)
+
+
+
